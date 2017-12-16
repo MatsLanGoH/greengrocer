7fd7ef562443a0d233c73cdab13e7c7b402ddcd6
Matthias Lambrecht <MatsLanGoH>
2017-12-16 15:14:49 +0900

    Add docstrings

--- sales/ledger.py
+++ sales/ledger.py
@@ -1,22 +1,21 @@
 from datetime import date
 
 
-class Ledger(object):
+class Ledger:
     """
-    TODO: docstring
-    this should hold all transactions for that date.
+    販売情報を登録し、合計金額、売上、内訳を返すためのクラス
     """
 
     def __init__(self):
         self.date = date.today()
-        self.total = 0  # TODO: Get amount from items instead
+        self.total = 0
         self.transactions = dict()
 
     def add_transactions(self, transactions):
         """
-        TODO docstring
-        :param transactions:
-        :return:
+        Ledgerに販売情報を登録する
+
+        :param transactions: Transactionのリスト
         """
         # Summarize transaction stats (num of sold items, amount) into Ledger instance
         for transaction in transactions:
@@ -29,31 +28,29 @@ class Ledger(object):
 
     def update_total(self):
         """
-        TODO docstring
-        :return:
+        Ledgerに登録されている販売情報から合計金額を更新する
         """
         total = 0
         for item in self.transactions.values():
             total += item.amount
         self.total = total
 
-    def set_date(self, dt):
+    def set_date(self, ledger_date):
         """
-        TODO docstring
-        :param dt:
-        :return:
+        Ledgerの年月日を設定する
+
+        :param ledger_date: 年月日 ex. datetime.date(2017, 12, 24)
         """
-        self.date = dt
+        self.date = ledger_date
 
     def __str__(self):
         """
-        TODO docstring
-        :return:
+        Ledgerに登録されている販売情報の内訳（登録商品の種類、数、累計）を返す
+
+        :return: 文字列（販売情報の内訳）
         """
         msg = ""
-        for fruit, transaction in self.transactions.items():
-            msg += "{fruit}: {amount}円({num_items}) ".format(fruit=fruit.label, amount=transaction.amount,
-                                                             num_items=transaction.num_items)
+        for product, transaction in self.transactions.items():
+            msg += "{product}: {amount}円({num_items}) ".format(product=product.label, amount=transaction.amount,
+                                                               num_items=transaction.num_items)
         return msg
-
-
